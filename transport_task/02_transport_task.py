1ba6b683fe0a335d7d92084ab04aabb2b9f24cf2
CandleStudent <rolltake@yandex.ru>
2024-12-04 01:53:47 +0300

    добавил что-то на врожденность + принты понатыкал

--- transport_task/02_transport_task.py
+++ transport_task/02_transport_task.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 from transport_task import BasicPlanFinder, OptimalPlanFinder
-from transport_task.BasicPlanFinder import MinimalCostMethod
+from transport_task.BasicPlanFinder import MinimalCostMethod, NorthWestCornerMethod
 from transport_task.MethodOfPotentials import MethodOfPotentials
 
 
@@ -24,17 +24,33 @@ class Task:
         self.plan = self.optimalPlanFinder.find_optimal_plan(self.basicPlanFinder.find_path())
 
 
+# costs1 = np.array([[4893, 4280, 6213], [5327, 4296, 6188], [6006, 5030, 7224]])
+# supply1 = np.array([1000, 1700, 1600])  # запасы
+# demand1 = np.array([1600,1000,1700])  # спрос
+# print(MinimalCostMethod(supply1, demand1, costs1).find_path())
+# task = Task(supply1,
+#             demand1,
+#             costs1,
+#             MinimalCostMethod(supply1, demand1, costs1),
+#             MethodOfPotentials(supply1, demand1, costs1))
+#
+# task.solve()
+# print(task.plan)
+
+
 supply = np.array([1300, 1200, 1100])
 demand = np.array([1000, 1500, 1100])
 cost = np.array([
     [4893, 4280, 6213],
     [5327, 4296, 6188],
     [6006, 5030, 7224]])
+print(MinimalCostMethod(supply, demand, cost).find_path())
 
 task = Task(supply,
             demand,
             cost,
-            MinimalCostMethod(supply, demand, cost),
+            NorthWestCornerMethod(supply, demand, cost),
             MethodOfPotentials(supply, demand, cost))
 
 task.solve()
+print(task.plan)
--- transport_task/MethodOfPotentials.py
+++ transport_task/MethodOfPotentials.py
@@ -12,15 +12,43 @@ class MethodOfPotentials(OptimalPlanFinder):
         self.max_delta = None
         self.max_delta_i_j = None
 
+    def print_matr(self):
+        for i in range(len(self.supply)):
+            print(self.optimal_plan[i])
+
     def find_optimal_plan(self, basic_plan:np.array):
+        print("Начало поиска оптимального плана")
         self.optimal_plan = basic_plan
+        print("Исходный план")
+        self.print_matr()
         self.initial_find_occupied_cells()
+        self.handle_degeneracy()
         self.find_potentials()
         while not self.is_plan_optimal():
             self.create_new_plan()
+            # self.handle_degeneracy() #кажется, не нужно, ибо всегда одна клетка убирается и одна прибавляется
+            print("Новый план")
+            self.print_matr()
             self.find_potentials()
 
         return self.optimal_plan
+
+    def handle_degeneracy(self):
+        # если базисных клеток меньше, чем m + n - 1
+        is_degenerated = len(self.__get_occupied_cells()) < len(self.supply) + len(self.demand) - 1
+        if is_degenerated:
+            print("План вырожденный")
+            print("Вводим фиктивную клетку")
+            for i in range(len(self.supply) - 1):
+                for j in range(len(self.demand)):
+                    if (self.occupied_cells[i, j] == 0) and self.occupied_cells[i + 1, j] != 0:
+                        self.occupied_cells[i, j] = 1
+                        if not (len(self.__get_occupied_cells()) < len(self.supply) + len(self.demand) - 1):
+                            print('Клетка: (', i, ',', j, ') = 0')
+                            print('\n* * * * * * * * * * * * * * * * *\n')
+                            return
+        else:
+            print("План невырожденный")
 
     def initial_find_occupied_cells(self):
         for i in range(len(self.supply)):
@@ -39,11 +67,18 @@ class MethodOfPotentials(OptimalPlanFinder):
             linear_equations_system_vars.append(linear_equation)
             linear_equations_system_consts.append(self.cost[indexes[0], indexes[1]])
         linear_equations_system_vars = np.array(linear_equations_system_vars)
+        self.print_potential_system(linear_equations_system_vars, linear_equations_system_consts)
         # устанавливаем значение одной из переменных как нуль (убираем один столбец по сути, считая его известным)
         linear_equations_system_vars = linear_equations_system_vars[:, 1:]
 
         self.potentials = np.linalg.solve(linear_equations_system_vars, linear_equations_system_consts)
         self.potentials = np.append(0, self.potentials) # добавляем зануленный потенциал
+        print("Потенциалы:", self.potentials)
+
+    def print_potential_system(self, coefs, consts):
+        print("Матрица системы для поиска потенциалов")
+        for i in range(len(coefs)):
+            print(coefs[i], consts[i])
 
     def __find_non_null_cells(self):
         non_null_cells = []
@@ -68,7 +103,7 @@ class MethodOfPotentials(OptimalPlanFinder):
                             if delta > self.max_delta:
                                 self.max_delta = delta
                                 self.max_delta_i_j = (i, j)
-
+        print("план оптимальный? ", is_plan_optimal)
         return is_plan_optimal
 
     def __get_demand_potential(self, index:int):
@@ -78,12 +113,16 @@ class MethodOfPotentials(OptimalPlanFinder):
         return self.potentials[index]
 
     def create_new_plan(self):
+        print("Создаем новый план")
+        print("Цикл начинается в клетке ", self.max_delta_i_j)
         # find cycle and put + and - in cycle_matrix
         loop = self.find_loop()
+        print("Найденный цикл: ", loop)
         even_cells = loop[0::2]  # четные позиции в цикле <==> +
         odd_cells = loop[1::2]  # нечетные позиции в цикле <==> -
         lower_transfer_i, lower_transfer_j = self.__get_lower_transfer_index(odd_cells) # из всех объемов перевозок с - -- наименьший
         lower_tranfer = self.optimal_plan[lower_transfer_i, lower_transfer_j]
+        print("Наименьший объем перевозок ", lower_tranfer, "В клетке ", lower_transfer_i, lower_transfer_j)
         # меняем матрицу занятости
         self.occupied_cells[lower_transfer_i, lower_transfer_j] = 0
         self.occupied_cells[self.max_delta_i_j[0], self.max_delta_i_j[1]] = 1
@@ -91,10 +130,15 @@ class MethodOfPotentials(OptimalPlanFinder):
         self.__update_optimal_plan_after_find_lower_tranfer(even_cells, odd_cells, lower_tranfer)
 
     def __update_optimal_plan_after_find_lower_tranfer(self, even_cells, odd_cells, lower_transfer):
+        count_null_cells = 0
         for even_cell in even_cells:
             self.optimal_plan[even_cell[0], even_cell[1]] += lower_transfer
         for odd_cell in odd_cells:
             self.optimal_plan[odd_cell[0], odd_cell[1]] -= lower_transfer
+            # кажется, не нужно, ибо там и так будет 1, ибо мы зануляем только одну клетку в методе выше
+            # count_null_cells += 1
+            # if count_null_cells > 1:
+            #     self.occupied_cells[odd_cell[0], odd_cell[1]] = 1
 
     def __get_lower_transfer_index(self, odd_cells: list[tuple[int, int]]):
         lower_transfer = self.optimal_plan[odd_cells[0]]
